378bd0658b66459d5c32e97e8c80ed06cd85dd37
el-proGit <elena.pro185@gmail.com>
2018-02-25 21:25:53 -0500

    done and done

--- new1.cpp
+++ new1.cpp
@@ -6,6 +6,7 @@ int main()
 	cout << "This is a test for Git" << end;
 	cout << "hello, I am using notepad++!" << endl;
 	cout << "So, now I'm just adding some more changes to this code." << endl;
+	cout << "some more changes..." << endl;
 	
 	return 0;
 }
