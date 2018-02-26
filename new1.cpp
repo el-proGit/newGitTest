1dd36ee3f0c45afca2e2bbe16a4c2e5c718e5763
el-proGit <elena.pro185@gmail.com>
2018-02-25 21:18:44 -0500

    This is a message for me to test github

--- new1.cpp
+++ new1.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	cout << "This is a test for Git" << end;
 	cout << "hello, I am using notepad++!" << endl;
+	cout << "So, now I'm just adding some more changes to this code." << endl;
 	
 	return 0;
 }
